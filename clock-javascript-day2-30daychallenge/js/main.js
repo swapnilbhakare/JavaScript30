81f2ad3691225f1805bed0810eb0fd0a21a16fde
swapnil bhakare <67636219+swapnilbhakare@users.noreply.github.com>
2020-10-03 10:22:44 +0530

    Add files via upload

---
+++ clock-javascript-day2-30daychallenge/js/main.js
@@ -0,0 +1,22 @@
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+	const now = new Date();
+
+	const secounds = now.getSeconds();
+	const secoundsDegrees =((secounds/60)* 360) +90;
+	secondHand.style.transform =`rotate(${secoundsDegrees}deg)`;
+
+	const mins = now.getMinutes();
+	const minsDegrees =((mins/60)*360) +90;
+	minsHand.style.transform =`rotate(${minsDegrees}deg)`;
+
+	const hour = now.getMinutes()
+	const hourDegrees =((mins/12)*360) +90;
+	hourHand.style.transform =`rotate(${hourDegrees}deg)`;
+
+
+}
+setInterval(setDate,1000)
